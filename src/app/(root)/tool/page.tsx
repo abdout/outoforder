7e0d3fac1697ae6cdd2b11e6ac459c22d06b4032
abdout <osmanabdout@hotmail.com>
2024-04-21 00:04:14 +0300

    x

--- src/app/(root)/tool/page.tsx
+++ src/app/(root)/tool/page.tsx
@@ -6,13 +6,13 @@ const Tool = () => {
   return (
     <div className="">
 
-      <p className="text-[11.5px] tracking-wide font-light md:text-lg md:pt-8">
+      <p className="text-[11.5px] tracking-wide font-light md:text-lg md:pt-3">
         The most magical part of the Harry Potter books, <br/> is that they eventually
         used the skills they learned at school
       </p>
       <div>
         <p className="text-[16px] md:pt-4 md:text-lg">+ 40 tool for</p>
-        <p className="text-[24px] md:pb-4">bussiness automation</p>
+        <p className="text-[28px] font-medium md:pb-4">bussiness automation</p>
       
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 md:gap-8 md:items-center md:justify-center ">
