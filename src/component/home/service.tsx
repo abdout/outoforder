356dd45efe05e820f087f6488b03d5b487a24960
abdout <osmanabdout@hotmail.com>
2024-04-21 20:37:08 +0300

    x

--- src/component/home/service.tsx
+++ src/component/home/service.tsx
@@ -6,7 +6,7 @@ const Service = () => {
     <div className='flex flex-col space-y-4 pt-8'>
         <p className='px-8 text-2xl'>We <strong>automate</strong> companies, healthcare, education, parties and beyond.</p>
         <div className='flex items-center justify-center gap-20 pt-4'>
-            <XlIcon src = '/home/company.png' alt = 'Company' path='' />
+            <XlIcon src = '/home/company.png' alt = 'Company' path='https://eco.databayt.org/home' />
             <XlIcon src = '/home/health.png' alt = 'Health' path='' />
             <XlIcon src = '/home/scholar.png' alt = 'Scholar' path='' />
             <XlIcon src = '/home/party.png' alt = 'Party' path='' />
