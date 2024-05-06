6d064ab89f1f3ec8ef1cc34c737ad9953eb8b223
abdout <osmanabdout@hotmail.com>
2024-05-06 14:54:37 +0300

    x

--- src/app/(root)/community/page.tsx
+++ src/app/(root)/community/page.tsx
@@ -1,13 +1,12 @@
 
 import Footer from "@/component/layout/footer";
-import Header from "@/component/layout/header";
+
 import React from "react";
 
 const Community = () => {
   return (
     <div className="space-y-4">
-      <Header />
-      <Footer />
+      Community
       
     </div>
   );
