909d58fc50c261af7228f3abcdb014cc0fba515f
abdout <osmanabdout@hotmail.com>
2024-06-14 10:01:36 +0300

    x

--- src/components/table/coloum.tsx
+++ src/components/table/coloum.tsx
@@ -1,9 +1,8 @@
 'use client'
 
+import React, { useEffect } from 'react'; // Import React and useEffect
 import { ColumnDef } from '@tanstack/react-table'
-
 import { MoreHorizontal, ArrowUpDown } from 'lucide-react'
-
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -14,12 +13,47 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { Icon } from '@iconify/react';
-
-
 import { member } from '../platform/member/type'
 import Link from 'next/link'
 import { useMember } from '../platform/member/context'
-import { useEffect } from 'react'
+
+const ActionsCell: React.FC<{ row: any }> = ({ row }) => {
+  const { refreshMembers, members, deleteMember } = useMember();
+  
+  useEffect(() => {
+    refreshMembers();
+    console.log(members);
+  }, []);
+
+  const user = row.original;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' className='h-8 w-8 p-0'>
+          <span className='sr-only'>Open menu</span>
+          <MoreHorizontal className='h-4 w-4' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuLabel>الضبط</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => navigator.clipboard.writeText(user._id)}
+        >
+          نسخ 
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href={`/member/${user._id}`}>
+            ملف
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>تجميد</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => user._id && deleteMember(user._id)}>حذف</DropdownMenuItem> 
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export const columns: ColumnDef<member>[] = [
   {
@@ -27,15 +61,14 @@ export const columns: ColumnDef<member>[] = [
     header: ({ column }) => {
       return (
         <div className="text-right">
-        <Button
-        className='p-0 m-0'
-        
-          variant='ghost'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          الاسم
-          <ArrowUpDown className=' h-4 w-4' />
-        </Button>
+          <Button
+            className='p-0 m-0'
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          >
+            الاسم
+            <ArrowUpDown className=' h-4 w-4' />
+          </Button>
         </div>
       )
     }
@@ -50,24 +83,19 @@ export const columns: ColumnDef<member>[] = [
   },
   {
     accessorKey: 'rank',
-    // header: () => <div className="text-right">التخصص</div>,
-    header: 'التخصص'
-    
+    header: 'التخصص'    
   },
   {
     accessorKey: 'skill',
-    header: () => <div className="text-right">المهارة</div>,
-    
+    header: () => <div className="text-right">المهارة</div>,    
   },
   {
     accessorKey: 'interest',
-    header: () => <div className="text-right">الاهتمام</div>,
-    
+    header: () => <div className="text-right">الاهتمام</div>,    
   },
   {
     accessorKey: 'dob',
-    header: () => <div className="text-right">العمر</div>,
-    
+    header: () => <div className="text-right">العمر</div>,    
   },
   {
     accessorKey: 'contact',
@@ -78,54 +106,18 @@ export const columns: ColumnDef<member>[] = [
       return (
         <div className='flex gap-4'>
           {phone && <a href={phone}>
-            <Icon icon='ph:phone-call-thin' width={24} 
-            className='reveal'/>
+            <Icon icon='ph:phone-call-thin' width={24} className='reveal'/>
             </a>}
-          {/* {facebook && <a href={facebook}  ><Icon icon='circum:mail' width={20} /></a>} */}
           {whatsapp && <a href={`https://wa.me/${whatsapp}`} target="_blank" rel="noopener noreferrer">
           <Icon icon='circum:mail' width={25} className='reveal' />
           </a>}
         </div>
-      )},
+      )
+    },
   },
   
-  // {
-  //   id: 'actions',
-  //   cell: ({ row }) => {
-  //     const user = row.original
-
-  //     const { refreshMembers, members, deleteMember } = useMember();
-  //     useEffect(() => {
-  //       refreshMembers();
-  //       console.log(members); // Add this line
-  //   }, []);
-
-  //     return (
-  //       <DropdownMenu>
-  //         <DropdownMenuTrigger asChild>
-  //           <Button variant='ghost' className='h-8 w-8 p-0'>
-  //             <span className='sr-only'>Open menu</span>
-  //             <MoreHorizontal className='h-4 w-4' />
-  //           </Button>
-  //         </DropdownMenuTrigger>
-  //         <DropdownMenuContent align='end'>
-  //           <DropdownMenuLabel>الضبط</DropdownMenuLabel>
-  //           <DropdownMenuSeparator />
-  //           <DropdownMenuItem
-  //             onClick={() => navigator.clipboard.writeText(user._id)}
-  //           >
-  //             نسخ 
-  //           </DropdownMenuItem>
-  //           <DropdownMenuItem>
-  //             <Link href={`/member/${user._id}`}>
-  //               ملف
-  //             </Link>
-  //           </DropdownMenuItem>
-  //           <DropdownMenuItem>تجميد</DropdownMenuItem>
-  //           <DropdownMenuItem onClick={() => user._id && deleteMember(user._id)}>حذف</DropdownMenuItem> 
-  //         </DropdownMenuContent>
-  //       </DropdownMenu>
-  //     )
-  //   }
-  
+  {
+    id: 'actions',
+    cell: ActionsCell // Use the new React component or custom hook function here
+  }
 ]
