1b848bbdcfa459688eb4d1d1bff309edaa0caf7f
abdout <osmanabdout@hotmail.com>
2024-06-29 13:33:23 +0300

    x

--- src/app/(contribute)/layout.tsx
+++ src/app/(contribute)/layout.tsx
@@ -1,13 +1,12 @@
-import { Rubik, Inter } from "next/font/google"; // Step 1: Import Inter
+import { Inter } from "next/font/google"; // Step 1: Import Inter
 import "../globals.css";
 
-export const rubik = Rubik({ subsets: ["latin"], variable: "--rubik" });
 const inter = Inter({ subsets: ["latin"] }); // Step 2: Initialize Inter
 
 export default function ContributeLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${rubik.className} ${inter.className}`} dir="ltr"> {/* Step 3: Apply both classNames */}
+      <body className={`${inter.className}`} dir="ltr"> {/* Step 3: Apply both classNames */}
         <div className="container">
           <div className="wrapper">{children}</div>
         </div>
